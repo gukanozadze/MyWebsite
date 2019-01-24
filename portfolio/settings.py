90103be1d5e1723cf14cd6b457d402f8f1397fa3
Guka Nozadze <gukanozadze@gmail.com>
2019-01-24 15:31:03 +0400

    added postgress db

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'django1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
@@ -128,3 +132,9 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+try:
+    from local_settings import * 
+except ImportError:
+    pass
