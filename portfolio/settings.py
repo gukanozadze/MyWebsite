bc271d31cd740b650a7569d6ff802eaf664c4d90
Guka Nozadze <gukanozadze@gmail.com>
2019-04-26 02:49:44 +0400

    added linkedin icon

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
