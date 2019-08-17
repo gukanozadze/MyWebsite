51fdf5f946b8c85917174ff9dfe8590f5a3ef2b4
gukanozadze <gukanozadze@gmail.com>
2019-08-17 05:15:30 +0400

    changed modal behaviour

--- portfolio/static/portfolio/js/footer.js
+++ portfolio/static/portfolio/js/footer.js
@@ -3,14 +3,23 @@ const bgModal = $('.bg-modal')
 bgModal.hide()
 
 $("#footer-contact").on('click', function(e){
-    e.preventDefault()
     $("body").css("overflow-y", "hidden");
 
     bgModal.show()
 })
 
+
+
+$('.bg-modal').on('click', function(e){
+    bgModal.hide()
+})
+
+
+$('.bg-modal__content').on('click', function(e){
+    e.stopPropagation();
+})
+
 $('.bg-modal__content--close').on('click', function(e){
-    e.preventDefault()
 
     $("body").css("overflow-y", "auto");
 
